9bc5c6e33a2ddd24bdaadd21ba8a70a8bd2677e5
commando2701 <totof2724@hotmail.com>
2022-10-22 15:36:07 -0400

    Suite
    suite

--- main2.py
+++ main2.py
@@ -1,4 +1,5 @@
 
+from msilib.schema import Font
 import random, sys, pygame, time, copy
 from pygame.locals import *
 
@@ -20,6 +21,15 @@ def main():
     MAINCLOCK = pygame.time.Clock()
     BGIMAGE = pygame.image.load('background.png')
     FONT = pygame.font.Font('freesansbold.ttf', 16)
+    
+    boardImage = pygame.image.load('board.png')
+    boardImage = pygame.transform.smoothscale(boardImage, (8 * 50, 8 * 50))
+
+    boardImageRect = boardImage.get_rect()
+    boardImageRect.topleft = (120, 120)
+    
+    BGIMAGE = pygame.transform.smoothscale(BGIMAGE, (640, 640))
+    BGIMAGE.blit(boardImage, boardImageRect)
 
     while True:
         if Jouerjeu() == False:
@@ -29,9 +39,7 @@ def main():
 def Jouerjeu():
 
     global tour
-    tableauPrincipal = getNouveauTableau()
-    print(tableauPrincipal)
-    
+    tableauPrincipal = getNouveauTableau()    
     razTableau(tableauPrincipal)
     dessinerTableau(tableauPrincipal)
     
@@ -64,9 +72,113 @@ def Jouerjeu():
                 MAINCLOCK.tick(60)
                 pygame.display.update()
             
-            #makeMove(tableauPrincipal, tuileJoueur, movexy[0], movexy[1], True)
+            faireMouvement(tableauPrincipal, tuileJoueur, movexy[0], movexy[1], True)
             if getMouvementValide(tableauPrincipal, tuileOrdi) != []:
                 tour = 'ordi'
+
+
+    dessinerTableau(tableauPrincipal)
+    scores = scoreTableau(tableauPrincipal)
+
+    if scores[tuileJoueur] > scores[tuileOrdi]:
+        text = 'vous avez battu lordinatueur par %s points! Félicitation!' % \
+               (scores[tuileJoueur] - scores[tuileOrdi])
+    elif scores[tuileJoueur] < scores[tuileOrdi]:
+        text = 'Vous avez perdu. lordinateur vous a battu par %s points.' % \
+               (scores[tuileOrdi] - scores[tuileJoueur])
+    else:
+        text = 'Partie Nul'
+
+    textSurf = FONT.render(text, True, BLANC, VERT)
+    textRect = textSurf.get_rect()
+    textRect.center = (int(640 / 2), int(640 / 2))
+    ecran.blit(textSurf, textRect)
+
+    text2Surf = Font.render('Jouer encore', True, BLANC, VERT)
+    text2Rect = text2Surf.get_rect()
+    text2Rect.center = (int(640 / 2), int(640 / 2) + 50)
+
+    yesSurf = Font.render('oui', True, BLANC, VERT)
+    yesRect = yesSurf.get_rect()
+    yesRect.center = (int(640 / 2) - 60, int(640 / 2) + 90)
+
+    noSurf = Font.render('non', True, BLANC, VERT)
+    noRect = noSurf.get_rect()
+    noRect.center = (int(640 / 2) + 60, int(640 / 2) + 90)
+
+    while True:
+        checkForQuit()
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONUP:
+                mousex, mousey = event.pos
+                if yesRect.collidepoint( (mousex, mousey) ):
+                    return True
+                elif noRect.collidepoint( (mousex, mousey) ):
+                    return False
+        ecran.blit(textSurf, textRect)
+        ecran.blit(text2Surf, text2Rect)
+        ecran.blit(yesSurf, yesRect)
+        ecran.blit(noSurf, noRect)
+        pygame.display.update()
+        MAINCLOCK.tick(60)
+
+
+def scoreTableau(grille):
+    xscore = 0
+    oscore = 0
+    for x in range(8):
+        for y in range(8):
+            if grille[x][y] == TUILE_BLANCHE:
+                xscore += 1
+            if grille[x][y] == TUILE_NOIRE:
+                oscore += 1
+    return {TUILE_BLANCHE:xscore, TUILE_NOIRE:oscore}
+
+
+def faireMouvement(grille, tuile, x, y, mouvement=False):
+   
+    tilesToFlip = isValidMove(grille, tuile, x, y)
+
+    if tilesToFlip == False:
+        return False
+
+    grille[x][y] = tuile
+
+    if mouvement:
+        animationChangementTuile(tilesToFlip, tuile, (x, y))
+
+    for x, y in tilesToFlip:
+        grille[x][y] = tuile
+    return True
+
+
+def animationChangementTuile(tuileFlip, tuileCouleur, autreTuile):
+
+    if tuileCouleur == TUILE_BLANCHE:
+        additionalTileColor = BLANC
+    else:
+        additionalTileColor = NOIR
+    additionalTileX, additionalTileY = 120 + autreTuile[0] * 50 + int(50 / 2), 120 + autreTuile[1] * 50 + int(50 / 2)
+    pygame.draw.circle(ecran, additionalTileColor, (additionalTileX, additionalTileY), int(50 / 2) - 4)
+    pygame.display.update()
+
+    for rgbValues in range(0, 255, int(25 * 2.55)):
+        if rgbValues > 255:
+            rgbValues = 255
+        elif rgbValues < 0:
+            rgbValues = 0
+
+        if tuileCouleur == TUILE_BLANCHE:
+            color = tuple([rgbValues] * 3)
+        elif tuileCouleur == TUILE_NOIRE:
+            color = tuple([255 - rgbValues] * 3)
+
+        for x, y in tuileFlip:
+            centerx, centery = 120 + x * 50 + int(50 / 2), 120 + y * 50 + int(50 / 2)
+            pygame.draw.circle(ecran, color, (centerx, centery), int(50 / 2) - 4)
+        pygame.display.update()
+        MAINCLOCK.tick(60)
+        checkForQuit()
 
 
 def getNouveauTableau():
@@ -178,7 +290,8 @@ def dessinerTableau(grille):
                 else:
                     tileColor = NOIR
                 pygame.draw.circle(ecran, tileColor, (centerx, centery), int(50 / 2) - 4)
-           
+
+
 def qui_commence():
     global tour
     BIGFONT = pygame.font.Font('freesansbold.ttf', 32)
@@ -202,11 +315,13 @@ def qui_commence():
                 mousex, mousey = event.pos
                 if xRect.collidepoint( (mousex, mousey) ):
                     tour = 'joueur'
+                    
                     return [TUILE_NOIRE, TUILE_BLANCHE]
                 elif oRect.collidepoint( (mousex, mousey) ):
                     tour = 'ordi'
                     return [TUILE_BLANCHE, TUILE_NOIRE]
 
+        print(tour)
         ecran.blit(textSurf, textRect)
         ecran.blit(xSurf, xRect)
         ecran.blit(oSurf, oRect)
